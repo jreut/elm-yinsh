0538c929de1d4029ff22fe683ad2304757b18fde
Jordan Ryan Reuter <me@jreut.com>
2017-07-03 10:57:24 -0400

    Add a new constructor for the view configuration

--- src/View/Board.elm
+++ src/View/Board.elm
@@ -6,26 +6,40 @@ module View.Board
 
 import Svg exposing (Svg, svg)
 import Svg.Attributes exposing (viewBox, width, height)
+import Svg.Events exposing (onClick)
 import Html exposing (Html)
 
 
-type Config datum
-    = JustSvg (datum -> Svg Never)
+type Config datum msg
+    = JustSvg (datum -> Svg msg)
+    | SvgWithMsg
+        { toSvg : datum -> Svg msg
+        , toMsg : datum -> msg
+        }
 
 
-toSvg : (datum -> Svg Never) -> Config datum
+toSvg : (datum -> Svg msg) -> Config datum msg
 toSvg =
     JustSvg
 
 
-view : Config datum -> List datum -> Html Never
+view : Config datum msg -> List datum -> Html msg
 view config data =
-    case config of
-        JustSvg toSvg ->
-            data
-                |> List.map toSvg
-                |> svg
-                    [ viewBox "-6 -6 12 12"
-                    , height "100vh"
-                    , width "100vw"
-                    ]
+    let
+        container =
+            svg
+                [ viewBox "-6 -6 12 12"
+                , height "100vh"
+                , width "100vw"
+                ]
+    in
+        case config of
+            JustSvg toSvg ->
+                data |> List.map toSvg |> container
+
+            SvgWithMsg { toSvg, toMsg } ->
+                let
+                    makeNode datum =
+                        Svg.g [ onClick (toMsg datum) ] [ toSvg datum ]
+                in
+                    data |> List.map makeNode |> container
